5f3b28be925021983d47d23880f8c47f86691feb
Tseng, Oliver <otseng@yahoo.com>
2021-01-09 20:13:43 -0500

    Catch global uncaught exceptions

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 import os, platform, logging
 import logging.handlers as handlers
 from themes import Themes
+from PySide2.QtWidgets import (QMessageBox)
 
 # File "config.py" is essential for running module "config"
 # Create file "config.py" if it is missing.
@@ -568,5 +569,10 @@ except:
 
 # Set indexes of history records
 setCurrentRecord()
+
+def global_excepthook(type, value, traceback):
+    logger.error("Uncaught exception", exc_info=(type, value, traceback))
+
+sys.excepthook = global_excepthook
 
 sys.exit(app.exec_())

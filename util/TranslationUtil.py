b91398f5eded74c777aad14e5a5fc7b5e538d3a4
Oliver Tseng <otseng@yahoo.com>
2021-02-19 00:28:53 -0500

    update

--- util/TranslationUtil.py
+++ util/TranslationUtil.py
@@ -10,18 +10,20 @@ class TranslationUtil:
         files = sorted(glob.glob("translations/Translation_*.py"))
         return [file[-8:-3] for file in files]
 
+    @staticmethod
+    def getSystemDefaultLanguage():
+        locale.setlocale(locale.LC_ALL, "")
+        return locale.getlocale(locale.LC_MESSAGES)[0]
+
 
 def test_getlistSupportedLanguages():
     for lang in TranslationUtil.getListSupportedLanguages():
         print(lang)
 
-def test_langDetect():
-    locale.setlocale(locale.LC_ALL, "")
-    print(locale.getlocale(locale.LC_MESSAGES)[0])
-    if hasattr(environ, 'LANG'):
-        print(environ['LANG'])
+def test_defaultLanguage():
+    print(TranslationUtil.getSystemDefaultLanguage())
 
 if __name__ == "__main__":
 
     # test_getlistSupportedLanguages()
-    test_langDetect()
+    test_defaultLanguage()

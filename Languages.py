9e27295d90be3b598b6c04c9bf8ff67a84ff7b38
Tseng, Oliver <otseng@yahoo.com>
2021-02-20 20:53:20 -0500

    greek

--- Languages.py
+++ Languages.py
@@ -16,6 +16,7 @@ class Languages:
         "English (US)": "en_US",
         "French Français": "fr",
         "German Deutsch": "de",
+        "Greek Νέα Ελληνικά": "el",
         "Hindi हिन्दी": "hi",
         "Korean 한국어": "ko",
         "Italian Italiano": "it",

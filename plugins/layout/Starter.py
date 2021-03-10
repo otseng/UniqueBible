2e625be83b0b583a6771fb299d88504dbc033352
Tseng, Oliver <otseng@yahoo.com>
2021-03-09 22:13:06 -0500

    Update Starter.py

--- plugins/layout/Starter.py
+++ plugins/layout/Starter.py
@@ -47,52 +47,9 @@ class Starter:
         for feature, action, shortcut in items:
             addMenuItem(subMenu, feature, self, action, shortcut)
         menu.addSeparator()
-
-        # 3rd column
-        menu = addMenu(menuBar, "controlPanel")
-        for index, shortcut in enumerate((sc.openControlPanelTab0, sc.openControlPanelTab1, sc.openControlPanelTab2, sc.openControlPanelTab3, sc.openControlPanelTab4)):
-            addMenuItem(menu, "cp{0}".format(index), self, lambda index=index, shortcut=shortcut: self.openControlPanelTab(index), shortcut)
-        menu.addSeparator()
-        addMenuItem(menu, "menu1_miniControl", self, self.manageMiniControl, sc.manageMiniControl)
-        tabs = ("bible", "translations", "commentaries", "lexicons", "dictionaries")
-        subMenu = addSubMenu(menu, "miniControlTabs")
-        for index, tab in enumerate(tabs):
-            addMenuItem(subMenu, tab, self, lambda index=index: self.openMiniControlTab(index))
-        menu.addSeparator()
-        addMenuItem(menu, "reloadResources", self, self.reloadResources)
-        addMenuItem(menu, "menu1_reload", self, self.reloadCurrentRecord, sc.reloadCurrentRecord)
-
-        # 4th column
-        menu = addMenu(menuBar, "menu8_resources")
-        subMenu = addSubMenu(menu, "folders")
-        items = (
-            ("menu8_marvelData", self.openMarvelDataFolder),
-            ("menu11_images", self.openImagesFolder),
-            ("menu11_music", self.openMusicFolder),
-            ("menu11_video", self.openVideoFolder),
-        )
-        for feature, action in items:
-            addMenuItem(subMenu, feature, self, action)
-        menu.addSeparator()
         subMenu = addSubMenu(menu, "add")
         items = (
             ("menu8_bibles", self.installMarvelBibles),
-            ("menu8_commentaries", self.installMarvelCommentaries),
-            ("menu8_datasets", self.installMarvelDatasets),
-            ("installBooks", self.installBooks),
-            ("hymn_lyrics", self.installHymnLyrics),
-            ("menu8_download3rdParty", self.moreBooks),
-        )
-        for feature, action in items:
-            addMenuItem(subMenu, feature, self, action)
-        menu.addSeparator()
-        subMenu = addSubMenu(menu, "import")
-        items = (
-            ("menu8_plusDictionaries", self.importBBPlusDictionaryInAFolder),
-            ("menu8_plusLexicons", self.importBBPlusLexiconInAFolder),
-            ("menu8_3rdParty", self.importModules),
-            ("menu8_3rdPartyInFolder", self.importModulesInFolder),
-            ("menu8_settings", self.importSettingsDialog),
         )
         for feature, action in items:
             addMenuItem(subMenu, feature, self, action)
@@ -100,10 +57,7 @@ class Starter:
         # information
         if config.showInformation:
             menu = addMenu(menuBar, "menu9_information")
-            addMenuItem(menu, "menu_keyboard_shortcuts", self, self.displayShortcuts, sc.displayShortcuts)
-            menu.addSeparator()
             addMenuItem(menu, "latestChanges", self, self.showInfo)
-            menu.addSeparator()
             subMenu = addSubMenu(menu, "menu_support")
             items = (
                 ("menu1_wikiPages", self.openUbaWiki, sc.ubaWiki),
@@ -112,7 +66,6 @@ class Starter:
             )
             for feature, action, shortcut in items:
                 addMenuItem(subMenu, feature, self, action, shortcut)
-            menu.addSeparator()
             subMenu = addSubMenu(menu, "websites")
             items = (
                 ("BibleTools.app", self.openBibleTools),
@@ -123,13 +76,11 @@ class Starter:
             )
             for feature, action in items:
                 addMenuItem(subMenu, feature, self, action, None, False)
-            menu.addSeparator()
             items = (
                 ("menu9_contact", self.contactEliranWong),
             )
             for feature, action in items:
                 addMenuItem(menu, feature, self, action)
-            menu.addSeparator()
             addMenuItem(menu, "menu9_donate", self, self.donateToUs)
 
     def setupToolBarStandardIconSize(self):

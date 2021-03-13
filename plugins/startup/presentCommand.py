d4fa957151c2e8629376c75eac6ba9c122816ba1
Tseng, Oliver <otseng@yahoo.com>
2021-03-12 22:49:37 -0500

    update

--- plugins/startup/presentCommand.py
+++ plugins/startup/presentCommand.py
@@ -6,9 +6,9 @@ from TextCommandParser import TextCommandParser
 # [KEYWORD] PRESENT
 # Shows passage in window for presentation purposes
 # Usage - PRESENT:::[BIBLE_VERSION]:::[BIBLE_REFERENCE(S)]
-# PRESENT:::NET:::John 3:16
+# PRESENT:::KJV:::John 3:16
 # Usage - PRESENT:::[BIBLE_VERSION]:::[BIBLE_REFERENCE(S)]:::[STYLE]
-# PRESENT:::NET:::John 3:16-20:::font-size:1.5em;margin-left:50px;margin-right:50px;
+# PRESENT:::NET:::John 3:16-17:::font-size:1.5em;margin-left:50px;margin-right:50px;color:magenta
 def presentCommand(command, source):
     logger = logging.getLogger('uba')
     msg = "Present passage {0}:{1}".format(command, source)

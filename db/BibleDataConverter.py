da6d603d419e5565831299d136b4ddf463d175ed
Tseng, Oliver <otseng@yahoo.com>
2021-04-04 23:09:18 -0400

    Update BibleDataConverter.py

--- db/BibleDataConverter.py
+++ db/BibleDataConverter.py
@@ -1,13 +1,120 @@
+import glob
 import os, re
+from pathlib import Path
 
 if __name__ == '__main__':
     from util.ConfigUtil import ConfigUtil
     ConfigUtil.setup()
 
+import config
+from BibleVerseParser import BibleVerseParser
+
 from ThirdParty import Converter
 
 
 class BibleDataConverter:
+
+    # Free Bible Version
+    # http://www.freebibleversion.org/
+    # https://ebible.org/find/details.php?id=engfbv
+    def convertFreeBibleVersion(self, directory):
+
+        if not os.path.isdir(directory):
+            print("{0} does not exist".format(directory))
+            exit()
+
+        text = "FBV"
+        description = "Free Bible Version"
+
+        bookNumDict = {
+            "1ch": "13",
+            "1co": "46",
+            "1jn": "62",
+            "1ki": "11",
+            "1pe": "60",
+            "1sa": "9",
+            "1th": "52",
+            "1ti": "54",
+            "2ch": "14",
+            "2co": "47",
+            "2jn": "63",
+            "2ki": "12",
+            "2pe": "61",
+            "2sa": "10",
+            "2th": "53",
+            "2ti": "55",
+            "3jn": "64",
+            "act": "44",
+            "amo": "30",
+            "col": "51",
+            "dan": "27",
+            "deu": "5",
+            "ecc": "21",
+            "eph": "49",
+            "est": "17",
+            "exo": "2",
+            "ezk": "26",
+            "ezr": "15",
+            "gal": "48",
+            "gen": "1",
+            "hab": "35",
+            "hag": "37",
+            "heb": "58",
+            "hos": "28",
+            "isa": "23",
+            "jas": "59",
+            "jdg": "7",
+            "jer": "24",
+            "jhn": "43",
+            "job": "18",
+            "jol": "29",
+            "jon": "32",
+            "jos": "6",
+            "jud": "65",
+            "lam": "25",
+            "lev": "3",
+            "luk": "42",
+            "mal": "39",
+            "mat": "40",
+            "mic": "33",
+            "mrk": "41",
+            "nam": "34",
+            "neh": "16",
+            "num": "4",
+            "oba": "31",
+            "phm": "57",
+            "php": "50",
+            "pro": "20",
+            "psa": "19",
+            "rev": "66",
+            "rom": "45",
+            "rut": "8",
+            "sng": "22",
+            "tit": "56",
+            "zec": "38",
+            "zep": "36",
+        }
+
+        versesData = []
+        for filename in glob.glob(directory+"/eng*.txt"):
+            f = Path(filename).stem
+            (bookName, chapterNum) = re.search(r".*_.*_(.*)_(.*)_.*", f).groups()
+            if bookName != "000":
+                bookName = bookName.lower()
+                bookNum = bookNumDict[bookName]
+                file = open(filename, "r")
+                lines = file.readlines()
+                count = 0
+                verseNum = 0
+                for line in lines:
+                    count += 1
+                    if count > 2 and len(line) > 1:
+                        verseNum += 1
+                        row = [bookNum, chapterNum, verseNum, line]
+                        versesData.append(row)
+
+        Converter().mySwordBibleToRichFormat(description, text, versesData)
+        Converter().mySwordBibleToPlainFormat(description, text, versesData)
 
     # New Heart Bible
     # https://nheb.net/
@@ -110,9 +217,18 @@ class BibleDataConverter:
         Converter().mySwordBibleToRichFormat(description, text, versesData)
         Converter().mySwordBibleToPlainFormat(description, text, versesData)
 
+    def generateDict(self):
+        bookNumDict = {}
+        parser = BibleVerseParser(config.parserStandarisation)
+        bookName = "Gen."
+        bookNum = parser.bookNameToNum(bookName)
+        bookNumDict[bookName] = bookNum
+        for key in bookNumDict.keys():
+            print('"{0}": "{1}",'.format(key, bookNumDict[key]))
+
 
 if __name__ == '__main__':
-
-    BibleDataConverter().convertNewHeartBible("/Users/otseng/Downloads/NHEB.txt")
+    config.useFastVerseParsing = True
+    BibleDataConverter().convertFreeBibleVersion("/Users/otseng/Downloads/engfbv_readaloud")
 
     print("Done")

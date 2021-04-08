726bafdae19153b27265eaf7f8e0c1901affcf24
Tseng, Oliver <otseng@yahoo.com>
2021-04-07 22:39:45 -0400

    Update GithubUtil.py

--- util/GithubUtil.py
+++ util/GithubUtil.py
@@ -1,7 +1,7 @@
 import logging
 from github import Github
 import config
-
+import base64
 
 class GithubUtil:
 
@@ -24,9 +24,17 @@ class GithubUtil:
             print(branch)
 
     def printContentsOfRepo(self):
-        contents = self.repo.get_contents("")
-        for contentFile in contents:
-            print(contentFile.name)
+        branch = self.repo.get_branch(self.repo.default_branch)
+        sha = branch.commit.sha
+        tree = self.repo.get_git_tree(sha)
+        for element in tree.tree:
+            print("{0}:{1}".format(element.path, element.sha))
+
+    def downloadFile(self, dir, filename, sha):
+        file = self.repo.get_git_blob(sha)
+        decoded = base64.b64decode(file.content)
+        with open(dir + "/" + filename, 'wb') as zipfile:
+            zipfile.write(decoded)
 
 
 if __name__ == "__main__":
@@ -35,4 +43,5 @@ if __name__ == "__main__":
 
     github = GithubUtil("darrelwright/UniqueBible_Commentaries")
     github.printContentsOfRepo()
+    github.downloadFile("output", "test.zip", "1274eccd33476b0e4716b41e292d50ad601715c8")
 

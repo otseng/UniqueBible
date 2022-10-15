bad7e290cc812ea689b914d34c20978beca27064
Oliver Tseng <otseng@yahoo.com>
2022-10-14 23:36:49 -0400

    Update RemoteApiHandler.py

--- util/RemoteApiHandler.py
+++ util/RemoteApiHandler.py
@@ -31,77 +31,16 @@ class ApiRequestHandler(SimpleHTTPRequestHandler):
 
 class RemoteApiHandler(ApiRequestHandler):
 
-    parser = None
-    textCommandParser = None
-    bibles = None
-    books = None
-    bookMap = None
-    abbreviations = None
-    session = None
-    users = []
-    adminUsers = []
-    whiteListFile = "ip_whitelist.txt"
-    whiteListIPs = []
-    blackListFile = "ip_blacklist.txt"
-    blackListIPs = []
+    jsonData = {}
 
     def __init__(self, *args, **kwargs):
         self.logger = logging.getLogger('uba')
-        if RemoteApiHandler.textCommandParser is None:
-            RemoteApiHandler.textCommandParser = TextCommandParser(RemoteCliMainWindow())
-        self.textCommandParser = RemoteApiHandler.textCommandParser
         config.internet = True
-        config.mainWindow = self
-        self.runStartupPlugins()
-        if RemoteApiHandler.bibles is None:
-            RemoteApiHandler.bibles = [(bible, bible) for bible in BiblesSqlite().getBibleList()]
-        self.bibles = RemoteApiHandler.bibles
-        if RemoteApiHandler.parser is None:
-            RemoteApiHandler.parser = BibleVerseParser(config.parserStandarisation)
-        self.parser = RemoteApiHandler.parser
-        if RemoteApiHandler.abbreviations is None:
-            RemoteApiHandler.abbreviations = self.parser.standardAbbreviation
-        self.abbreviations = RemoteApiHandler.abbreviations
-        if RemoteApiHandler.books is None:
-            RemoteApiHandler.books = [(k, v) for k, v in self.abbreviations.items() if int(k) <= 69]
-            RemoteApiHandler.bookMap = {k: v for k, v in self.abbreviations.items() if int(k) <= 69}
-        self.books = RemoteApiHandler.books
-        self.bookMap = RemoteApiHandler.bookMap
-        self.users = RemoteApiHandler.users
-        self.adminUsers = RemoteApiHandler.adminUsers
-        self.primaryUser = False
-        if config.httpServerViewerGlobalMode:
-            try:
-                urllib.request.urlopen(config.httpServerViewerBaseUrl)
-            except:
-                config.httpServerViewerGlobalMode = False
-        try:
-            if os.path.exists(self.whiteListFile):
-                self.whiteListIPs = [ip.strip() for ip in open(self.whiteListFile, "r").readlines()]
-            else:
-                Path(self.whiteListFile).touch()
-            if os.path.exists(self.blackListFile):
-                self.blackListIPs = [ip.strip() for ip in open(self.blackListFile, "r").readlines()]
-        except Exception as ex:
-            print("Could not read white/blacklists")
-            print(ex)
         try:
             super().__init__(*args, directory="htmlResources", **kwargs)
         except Exception as ex:
             print("Could not run init")
             print(ex)
-
-    def runStartupPlugins(self):
-        config.bibleWindowContentTransformers = []
-        config.customCommandShortcuts = {}
-        if config.enablePlugins:
-            for plugin in FileUtil.fileNamesWithoutExtension(os.path.join("plugins", "startup"), "py"):
-                if not plugin in config.excludeStartupPlugins:
-                    script = os.path.join(os.getcwd(), "plugins", "startup", "{0}.py".format(plugin))
-                    config.mainWindow.execPythonFile(script)
-
-    def execPythonFile(self, script):
-        self.textCommandParser.parent.execPythonFile(script)
 
     def do_POST(self):
         self.handleBadRequests()
@@ -110,11 +49,11 @@ class RemoteApiHandler(ApiRequestHandler):
         self.handleBadRequests()
 
     def handleBadRequests(self):
-        jsonData = {'status': 'Error', 'message': 'Unsupported method'}
-        self.sendJson(jsonData)
+        self.jsonData = {'status': 'Error', 'message': 'Unsupported method'}
+        self.sendJsonData()
 
-    def sendJson(self, jsonData):
-        data = json.dumps(jsonData)
+    def sendJsonData(self):
+        data = json.dumps(self.jsonData)
         self.commonHeader()
         self.wfile.write(bytes(data, "utf8"))
 
@@ -128,21 +67,33 @@ class RemoteApiHandler(ApiRequestHandler):
         self.end_headers()
 
     def send_error(self, code, message=None, explain=None):
-        jsonData = {'status': 'Error', 'message': f'{message}'}
-        data = json.dumps(jsonData)
-        self.commonHeader()
-        self.wfile.write(bytes(data, "utf8"))
+        self.jsonData = {'status': 'Error', 'message': f'{message}'}
+        self.sendJsonData()
 
     def do_GET(self):
         try:
             self.clientIP = self.client_address[0]
-            jsonData = self.processRequest(self.path)
+            self.processRequest(self.path)
         except Exception as ex:
-            jsonData = {'status': 'Error', 'exception': f'{ex}'}
-        self.sendJson(jsonData)
+            self.jsonData = {'status': 'Error', 'exception': ex}
+        self.sendJsonData()
 
     def processRequest(self, request):
-        query_components = parse_qs(urlparse(request).query)
-        jsonData = {'status': "OK"}
-        return jsonData
+        query = parse_qs(urlparse(request).query)
+        self.jsonData = {'status': "OK"}
+        if "?" in request:
+            request = request.split("?")[0]
+        self.jsonData['request'] = request
+        if query:
+            self.jsonData['query'] = query
+        cmd = request[1:].split("/")
+        if len(cmd) > 0:
+            if cmd[0].lower() == "list":
+                self.processListCommand(cmd)
+
+    def processListCommand(self, cmd):
+        self.jsonData['data'] = cmd[1]
+        if cmd[1].lower() == "bibles":
+            pass
+
 

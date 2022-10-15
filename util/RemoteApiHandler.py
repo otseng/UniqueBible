c35c54fc716b906de080116758efebbfa0651a0a
Oliver Tseng <otseng@yahoo.com>
2022-10-15 08:02:26 -0400

    Update RemoteApiHandler.py

--- util/RemoteApiHandler.py
+++ util/RemoteApiHandler.py
@@ -5,9 +5,9 @@ from http import HTTPStatus
 
 from http.server import SimpleHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
-
 from db.BiblesSqlite import BiblesSqlite
 from db.ToolsSqlite import Commentary, LexiconData, IndexesSqlite
+from util.BibleBooks import BibleBooks
 
 
 class ApiRequestHandler(SimpleHTTPRequestHandler):
@@ -55,15 +55,18 @@ class RemoteApiHandler(ApiRequestHandler):
         self.end_headers()
 
     def send_error(self, code, message=None, explain=None):
-        self.jsonData = {'status': 'Error', 'message': f'{message}'}
+        self.jsonData = {'status': 'Error', 'message': message}
         self.sendJsonData()
+
+    def sendError(self, message):
+        self.send_error("", message)
 
     def do_GET(self):
         try:
             self.clientIP = self.client_address[0]
             self.processRequest(self.path)
         except Exception as ex:
-            self.jsonData = {'status': 'Error', 'exception': ex}
+            self.jsonData = {'status': 'Error', 'exception': str(ex)}
         self.sendJsonData()
 
     def processRequest(self, request):
@@ -76,11 +79,30 @@ class RemoteApiHandler(ApiRequestHandler):
             self.jsonData['query'] = query
         cmd = request[1:].split("/")
         if len(cmd) > 0:
-            if cmd[0].lower() == "list":
+            if cmd[0].lower() == "data":
+                self.processDataCommand(cmd, query)
+            elif cmd[0].lower() == "list":
                 self.processListCommand(cmd)
+            elif cmd[0].lower() == "bible":
+                self.processBibleCommand(cmd)
 
+    # /data/bible/abbreviations?lang=[eng,sc,tc]
+    def processDataCommand(self, cmd, query):
+        if cmd[1].lower() == "bible":
+            if cmd[2].lower() == "abbreviations":
+                lang = "eng"
+                if query:
+                    lang = query["lang"][0]
+                data = []
+                for key, value in BibleBooks().abbrev[lang].items():
+                    data.append({'i': key, 'a': value[0], 'n': value[1]})
+                self.jsonData['data'] = data
+
+    # /list/bibles
+    # /list/commentaries
+    # /list/lexicons
+    # /list/dictionaries
     def processListCommand(self, cmd):
-        # /list/bibles
         if cmd[1].lower() == "bibles":
             self.jsonData['data'] = [bible for bible in BiblesSqlite().getBibleList()]
         elif cmd[1].lower() == "commentaries":
@@ -89,6 +111,24 @@ class RemoteApiHandler(ApiRequestHandler):
             self.jsonData['data'] = [lexicon for lexicon in LexiconData().lexiconList]
         elif cmd[1].lower() == "dictionaries":
             self.jsonData['data'] = [dictionary[0] for dictionary in IndexesSqlite().dictionaryList]
+
+    # /bible/KJV/43/3
+    # /bible/KJV/44/3/16
+    def processBibleCommand(self, cmd):
+        if len(cmd) < 4:
+            self.sendError("Invalid command")
+            return
+        if len(cmd) == 4:
+            verses = BiblesSqlite().readTextChapter(cmd[1], cmd[2], cmd[3])
+        elif len(cmd) == 5:
+            verses = [BiblesSqlite().readTextVerse(cmd[1], cmd[2], cmd[3], cmd[4])]
+        rows = []
+        for verse in verses:
+            rows.append({'b': verse[0], 'c': verse[1], 'v': verse[2], 't': verse[3]})
+        self.jsonData['data'] = rows
+
+
+
 
 
 

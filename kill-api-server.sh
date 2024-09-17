5366ae880be92e0eac36c9c36b5016a5bb7601fb
Oliver Tseng <otseng@yahoo.com>
2024-09-17 07:39:59 -0400

    Fix kill scripts

--- kill-api-server.sh
+++ kill-api-server.sh
@@ -1,2 +1,5 @@
-ps -ef | grep api-server |  grep -v grep | awk '{print $2}' | xargs kill
+PROCESS=`ps -ef | grep api-server |  grep -v grep | awk '{print $2}'`
+if [ "$PROCESS" != "" ]; then
+  echo $PROCESS | xargs kill
+fi
 ps -ef | grep api-server 
--- kill-http-server.sh
+++ kill-http-server.sh
@@ -1,2 +1,6 @@
-ps -ef | grep http-server |  grep -v grep | awk '{print $2}' | xargs kill
+PROCESS=`ps -ef | grep http-server |  grep -v grep | awk '{print $2}'`
+echo $PROCESS
+if [ "$PROCESS" != "" ]; then
+  echo $PROCESS | xargs kill
+fi
 ps -ef | grep http-server 
